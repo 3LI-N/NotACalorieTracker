7f2a7ba30c202bffbc6cd11b69a07fba8d2dd377
3LI-N <enordan1881@gmail.com>
2023-04-17 21:24:31 -0400

    displays daily value percentages

---
+++ lib/dailyvaluelist.dart
@@ -0,0 +1,228 @@
+class NutrientDVList {
+  final String displayName;
+  final String usdaName;
+  final double dailyValue;
+  final String unitName;
+
+  NutrientDVList({
+    required this.displayName,
+    required this.usdaName,
+    required this.dailyValue,
+    required this.unitName,
+  });
+
+  List<NutrientDVList> nutrientDVList() {
+    return [
+      NutrientDVList(
+        displayName: "Calcium",
+        usdaName: "Calcium, Ca",
+        dailyValue: 1300,
+        unitName: "mg",
+      ),
+      NutrientDVList(
+        displayName: "Dietary Fiber",
+        usdaName: "Fiber, total dietary",
+        dailyValue: 28,
+        unitName: "g",
+      ),
+      NutrientDVList(
+        displayName: "Fat",
+        usdaName: "Total lipid (fat)",
+        dailyValue: 78,
+        unitName: "g",
+      ),
+      NutrientDVList(
+        displayName: "Magnesium",
+        usdaName: "Magnesium, Mg",
+        dailyValue: 420,
+        unitName: "mg",
+      ),
+      NutrientDVList(
+        displayName: "Manganese",
+        usdaName: "Manganese, Mn",
+        dailyValue: 2.3,
+        unitName: "mg",
+      ),
+      NutrientDVList(
+        displayName: "Phosphorus",
+        usdaName: "Phosphorus, P",
+        dailyValue: 1250,
+        unitName: "mg",
+      ),
+      NutrientDVList(
+        displayName: "Potassium",
+        usdaName: "Potassium, K",
+        dailyValue: 4700,
+        unitName: "mg",
+      ),
+      NutrientDVList(
+        displayName: "Vitamin C",
+        usdaName: "Vitamin C, total ascorbic acid",
+        dailyValue: 90,
+        unitName: "mg",
+      ),
+      NutrientDVList(
+        displayName: "Vitamin D",
+        usdaName: "Vitamin D (D2 + D3)",
+        dailyValue: 20,
+        unitName: "µg",
+      ),
+      NutrientDVList(
+        displayName: "Vitamin K",
+        usdaName: "Vitamin K (phylloquinone)",
+        dailyValue: 120,
+        unitName: "µg",
+      ),
+      NutrientDVList(
+        displayName: "Biotin",
+        usdaName: "",
+        dailyValue: 30,
+        unitName: "µg",
+      ),
+      NutrientDVList(
+        displayName: "Chloride",
+        usdaName: "",
+        dailyValue: 2300,
+        unitName: "mg",
+      ),
+      NutrientDVList(
+        displayName: "Chromium",
+        usdaName: "",
+        dailyValue: 35,
+        unitName: "µg",
+      ),
+      NutrientDVList(
+        displayName: "Copper",
+        usdaName: "Copper, Cu",
+        dailyValue: 0.9,
+        unitName: "mg",
+      ),
+      NutrientDVList(
+        displayName: "Folate/Folic Acid",
+        usdaName: "",
+        dailyValue: 400,
+        unitName: "µg",
+      ),
+      NutrientDVList(
+        displayName: "Molybdenum",
+        usdaName: "",
+        dailyValue: 45,
+        unitName: "µg",
+      ),
+      NutrientDVList(
+        displayName: "Niacin",
+        usdaName: "Niacin",
+        dailyValue: 16,
+        unitName: "mg",
+      ),
+      NutrientDVList(
+        displayName: "Pantothenic Acid",
+        usdaName: "Pantothenic acid",
+        dailyValue: 5,
+        unitName: "mg",
+      ),
+      NutrientDVList(
+        displayName: "Riboflavin",
+        usdaName: "Riboflavin",
+        dailyValue: 1.3,
+        unitName: "mg",
+      ),
+      NutrientDVList(
+        displayName: "Selenium",
+        usdaName: "Selenium, Se",
+        dailyValue: 55,
+        unitName: "µg",
+      ),
+      NutrientDVList(
+        displayName: "Sodium",
+        usdaName: "Sodium, Na",
+        dailyValue: 2300,
+        unitName: "mg",
+      ),
+      NutrientDVList(
+        displayName: "Thiamin",
+        usdaName: "Thiamin",
+        dailyValue: 1.2,
+        unitName: "mg",
+      ),
+      NutrientDVList(
+        displayName: "Carbohydrate",
+        usdaName: "Carbohydrate, by difference",
+        dailyValue: 275,
+        unitName: "g",
+      ),
+      NutrientDVList(
+        displayName: "Vitamin A",
+        usdaName: "Vitamin A, RAE",
+        dailyValue: 900,
+        unitName: "µg",
+      ),
+      NutrientDVList(
+        displayName: "Vitamin B-6",
+        usdaName: "Vitamin B-6",
+        dailyValue: 1.7,
+        unitName: "mg",
+      ),
+      NutrientDVList(
+        displayName: "Vitamin B-12",
+        usdaName: "Vitamin B-12",
+        dailyValue: 2.4,
+        unitName: "µg",
+      ),
+      NutrientDVList(
+        displayName: "Vitamin E",
+        usdaName: "Vitamin E (alpha-tocopherol)",
+        dailyValue: 15,
+        unitName: "mg",
+      ),
+      NutrientDVList(
+        displayName: "Zinc",
+        usdaName: "Zinc, Zn",
+        dailyValue: 11,
+        unitName: "mg",
+      ),
+      NutrientDVList(
+        displayName: "Cholesterol",
+        usdaName: "Cholesterol",
+        dailyValue: 300,
+        unitName: "mg",
+      ),
+      NutrientDVList(
+        displayName: "Iodine",
+        usdaName: "",
+        dailyValue: 150,
+        unitName: "µg",
+      ),
+      NutrientDVList(
+        displayName: "Iron",
+        usdaName: "Iron, Fe",
+        dailyValue: 18,
+        unitName: "mg",
+      ),
+      NutrientDVList(
+        displayName: "Protein",
+        usdaName: "Protein",
+        dailyValue: 50,
+        unitName: "g",
+      ),
+      NutrientDVList(
+        displayName: "Saturated fat",
+        usdaName: "Fatty acids, total saturated",
+        dailyValue: 20,
+        unitName: "g",
+      ),
+      NutrientDVList(
+        displayName: "Added sugars",
+        usdaName: "",
+        dailyValue: 50,
+        unitName: "g",
+      ),
+      NutrientDVList(
+        displayName: "Choline",
+        usdaName: "Choline, total",
+        dailyValue: 550,
+        unitName: "mg",
+      ),
+    ];
+  }
+}
--- lib/user_profile.dart
+++ lib/user_profile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'provider/nutrient_provider.dart';
 import '/models/usernutrientmodel.dart';
+import 'dailyvaluelist.dart';
 
 import 'detail_screen.dart';
 
@@ -12,11 +13,22 @@ class UserProfile extends StatefulWidget {
   _UserProfileState createState() => _UserProfileState();
 }
 
-class _UserProfileState extends State<UserProfile>{
+class _UserProfileState extends State<UserProfile> {
+  var nutrientDVList = NutrientDVList(
+      displayName: '', usdaName: '', dailyValue: 0, unitName: '');
+  List<NutrientDVList> _nutrientDVList = [];
+
+  @override
+  void initState() {
+    _nutrientDVList = nutrientDVList.nutrientDVList();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     var nutrientState = context.watch<NutrientProvider>();
-    var userNutrients = nutrientState.getUserData.nutrientList; // map with string as key and UserNutrient as value
+    var userNutrients = nutrientState.getUserData
+        .nutrientList; // map with string as key and UserNutrient as value
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -25,43 +37,58 @@ class _UserProfileState extends State<UserProfile>{
         ),
       ),
       body: Column(
-                children: [
-                  Container(
-                    child: Expanded(
-                      child: ListView.builder(
-                          itemCount: userNutrients.length,
-                          itemBuilder: (context, index) {
-                            String key = userNutrients.keys.elementAt(index);
-                            UserNutrient nutrient = userNutrients[key];
-                            return Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: FittedBox(
-                                    alignment: Alignment.topLeft,
-                                    fit: BoxFit.scaleDown,
-                                    child: Text(
-                                      "${nutrient.name}:   ${nutrient.amount} ${nutrient.unitName}",
-                                      style: TextStyle(
-                                              fontSize: 20,
-                                              fontWeight: FontWeight.w600,
-                                            ),
+        children: [
+          Container(
+            child: _nutrientDVList.isEmpty
+                ? const Center(
+                    child: CircularProgressIndicator(
+                    color: Colors.black,
+                  ))
+                : Expanded(
+                    child: ListView.builder(
+                        itemCount:
+                            _nutrientDVList.length, // was userNutrients.length
+                        itemBuilder: (context, index) {
+                          //String key = userNutrients.keys.elementAt(index);
+                          //UserNutrient nutrient = userNutrients[key];
+                          NutrientDVList nutrient = _nutrientDVList[index];
+                          double percentDV = 0.0;
+                          if (userNutrients.containsKey(nutrient.usdaName) &&
+                              userNutrients[nutrient.usdaName] != null) {
+                            percentDV =
+                                userNutrients[nutrient.usdaName].amount *
+                                    100 /
+                                    nutrient.dailyValue;
+                          }
+                          return Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: FittedBox(
+                                  alignment: Alignment.topLeft,
+                                  fit: BoxFit.scaleDown,
+                                  child: Text(
+                                    "${nutrient.displayName}:   ${percentDV.toStringAsFixed(3)}% daily value",
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w600,
                                     ),
                                   ),
                                 ),
-                                Container(
-                                  height: 1,
-                                  color: Colors.black54,
-                                ),
-                              ],
-                            );
-                          }),
-                    ),
+                              ),
+                              Container(
+                                height: 1,
+                                color: Colors.black54,
+                              ),
+                            ],
+                          );
+                        }),
                   ),
-                ],
-              ),
+          ),
+        ],
+      ),
     );
   }
 }
